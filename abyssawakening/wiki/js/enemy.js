adf44ff879444262d32db36cd8953c47c6f63093
elrias <ahamon47@gmail.com>
2026-02-26 22:41:47 +0100

    enemy stats

--- abyssawakening/wiki/js/enemy.js
+++ abyssawakening/wiki/js/enemy.js
@@ -36,3 +36,52 @@ function renderEnemyHeader(enemy) {
         this.onerror = null;
     };
 }
+
+function renderParameters(enemy) {
+
+    const container = document.getElementById("enemyParams");
+    container.innerHTML = "";
+
+    const p = enemy.params;
+
+    addParam("Max HP", p[0]);
+    addParam("ATK", p[2]);
+    addParam("MAT", p[4]);
+    addParam("DEF", p[3]);
+    addParam("MDF", p[5]);
+    addParam("EXP", enemy.exp);
+
+    // === SP (TPSphereCount) ===
+    const spMatch = enemy.note.match(/<TPSphereCount:\s*(\d+)>/);
+
+    if (spMatch) {
+        const count = Number(spMatch[1]);
+
+        if (count > 0) {
+
+            const div = document.createElement("div");
+            div.className = "param-line";
+
+            const orbs = Array.from({length: count})
+                .map(() => `<div class="sp-orb"></div>`)
+                .join("");
+
+            div.innerHTML = `
+                <span>SP</span>
+                <span class="sp-spheres">${orbs}</span>
+            `;
+
+            container.appendChild(div);
+        }
+    }
+
+    function addParam(name, value) {
+        const div = document.createElement("div");
+        div.className = "param-line";
+        div.innerHTML = `
+            <span>${name}</span>
+            <span>${value}</span>
+        `;
+        container.appendChild(div);
+    }
+}
