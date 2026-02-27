6cc98fe42097a344f5d4c7e02823f272bbe11283
elrias <ahamon47@gmail.com>
2026-02-27 13:04:55 +0100

    fixed bugs

--- abyssawakening/js/plugins/ElusiveRythm.js
+++ abyssawakening/js/plugins/ElusiveRythm.js
@@ -6,8 +6,19 @@
   Game_BattlerBase.prototype.xparam = function(xparamId) {
     let value = _xparam.call(this, xparamId);
 
+    // EVA
     if (xparamId === 1 && this.isStateAffected(STATE_ID)) {
-      value += this.xparam(2);
+
+      // On crée une action factice pour calculer le vrai crit
+      const action = new Game_Action(this);
+
+      // Important : on met une skill valide (attaque normale)
+      action.setSkill(this.attackSkillId());
+
+      // On utilise self comme cible fictive (la CEV n'importe pas ici)
+      const realCrit = action.itemCri(this);
+
+      value += realCrit;
     }
 
     return value;
