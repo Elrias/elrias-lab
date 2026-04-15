6364e1a4cc83d1778c547ba2e76bc7560bfe1b99
elrias <ahamon47@gmail.com>
2026-04-15 22:20:52 +0200

    fixed double row

--- abyssawakening/leaderboards/leaderboards.js
+++ abyssawakening/leaderboards/leaderboards.js
@@ -20,25 +20,26 @@ function goToProfile(username) {
 function renderLeaderboard(players) {
   const container = document.getElementById("leaderboard");
 
-  container.innerHTML = players.map((p, index) => `
-    <div class="leaderboard-row" onclick="goToProfile('${p.username}')">  
-        <div class="leaderboard-row rank-${index + 1}">
-        
+    container.innerHTML = players.map((p, index) => `
+    <div 
+        class="leaderboard-row rank-${index + 1}" 
+        onclick="goToProfile('${p.username}')"
+    >
+
         <div class="rank">#${index + 1}</div>
 
         <img 
-            class="avatar rank-${index + 1}" 
-            src="${p.avatar_url || BASE_PATH + "img/avatars/default.png"}"
-            onerror="this.src='${BASE_PATH}img/avatars/default.png'"
-            >
+        class="avatar rank-${index + 1}" 
+        src="${p.avatar_url || BASE_PATH + "img/avatars/default.png"}"
+        onerror="this.src='${BASE_PATH}img/avatars/default.png'"
+        >
 
         <div class="name">${p.username}</div>
 
         <div class="score">${p.score} pts</div>
 
-        </div>
     </div>
-  `).join("");
+    `).join("");
 }
 
 document.addEventListener("DOMContentLoaded", loadLeaderboard);
